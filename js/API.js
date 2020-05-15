cb039173743cec78e8ab7b5783908e4916d8768b
manu <manu.villar.vitoria@gmail.com>
2020-05-15 19:47:13 +0200

    consulting the API

--- js/API.js
+++ js/API.js
@@ -1,21 +1,17 @@
-class UI {
-    constructor() {
+class API {
 
-         // Iniciar el mapa
-         this.mapa = this.inicializarMapa();
+    async obtenerDatos() {
 
-    }
+        const total = 11000;
+         //Añadimos al final del link de la API: ?pageSize=${total}, ya que en la respuesta del objeto
+         // vemos: pagination: {pageSize: 11000, page: 1, total: 10018}. Y por tanto añadimos esta propiedad.
+         //Al mezclar propiedades, cambiamos las comillas sencillas por template strings.
+        const datos = await fetch (`https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize=${total}`);
 
-    inicializarMapa() {
-         // Inicializar y obtener la propiedad del mapa
-         const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
-         const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-         L.tileLayer(
-             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-             attribution: '&copy; ' + enlaceMapa + ' Contributors',
-             maxZoom: 18,
-             }).addTo(map);
-         return map;
+        const respuestaJSON = await datos.json();
 
+        return {
+            respuestaJSON
+        }
     }
 }
